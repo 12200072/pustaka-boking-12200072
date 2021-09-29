74fa2b6e596eeb754e8658486b581dd1fcf3701e
12200072 <randafebriansyah32@gmail.com>
2021-09-29 13:09:22 +0700

    tugas pertemuan 2

---
+++ app/Views/halaman/login.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Perpustakaan Login</title>
+    </head>
+    <body>
+
+        <h1>Selamat datang di Sistem informasi Perpustakaan</h1>
+        <p>
+            ini adalah layanan perpustakaan digital masa kini
+            <a href="<?=base_url( relativePath: '/daftar-member')?>">klik daftar</a>untuk mendaftar menjadi daftar_member
+     </p>
+
+    </body>
+</html>
+    
+            
