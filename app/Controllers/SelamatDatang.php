86463100a5def98ebc20e0b6054efc13e9de6aef
12200072 <randafebriansyah32@gmail.com>
2021-09-29 17:49:07 +0700

    tugas pertemuan 2

--- app/Controllers/SelamatDatang.php
+++ app/Controllers/SelamatDatang.php
@@ -3,16 +3,16 @@ namespace App\Controllers;
 
 class SelamatDatang extends BaseController {
 
+
     public function hal_awal(){
         return 'hello saya belajar CI4';
     }
 
     public function beranda_login(){
-    return view( name: 'halaman/login');
+    return view('halaman/login');
     }
 
     public function daftar_member(){
-    return view( name: 'halaman/daftar_member');
+    return view('halaman/daftar_member');
     }
 }
-
